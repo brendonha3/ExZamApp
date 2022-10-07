94de00b868000af067693d843d3f9ca068c6267c
boboray202 <52871221+boboray202@users.noreply.github.com>
2022-10-07 13:10:01 -0400

    created new classes

---
+++ GameTracker/GameTracker/GameTracker/Models/GameParams.cs
@@ -0,0 +1,8 @@
+﻿namespace GameTracker.Models
+{
+    public class GameDescription
+    {
+        public string? Genres { get; set; }
+        public string? Platforms { get; set; }
+    }
+}
---
+++ GameTracker/GameTracker/GameTracker/Models/GameSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace GameTracker.Models
+{
+    public class GameSettings
+    {
+        [PrimaryKey]
+        public long GameId { get; set; }
+        public bool? IsFavorite { get; set; }
+        public bool? IsWishlist { get; set; }
+        public bool? IsOwned { get; set; }
+        public DateTime? LastFavorite { get; set; }
+        public DateTime? LastWishlist { get; set; }
+        public DateTime? LastOwned { get; set; }
+    }
+}
